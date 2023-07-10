02ace1fb6f131ce8d13f05af85b88a816c8838b8
haneujeen <eujeenhan@gmail.com>
2023-07-10 21:08:02 +0900

    Add screens to navigator

--- react-native/App.js
+++ react-native/App.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 import MapLikeScreen from './screens/MapLikeScreen';
+import BusStationScreen from './screens/BusStationScreen';
+import SubwayStationScreen from './screens/SubwayStationScreen';
 
 // Create the stack navigator
 const Stack = createStackNavigator();
@@ -13,6 +15,8 @@ export default function App() {
             <Stack.Navigator initialRouteName="Home">
                 <Stack.Screen name="Home" component={HomeScreen} />
                 <Stack.Screen name="MapLike" component={MapLikeScreen} />
+                <Stack.Screen name="BusStation" component={BusStationScreen} />
+                <Stack.Screen name="SubwayStation" component={SubwayStationScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
