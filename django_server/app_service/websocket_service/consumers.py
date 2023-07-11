89a3dc9431cfc43695672eeeeeb8af7fdf5da91f
haneujeen <eujeenhan@gmail.com>
2023-07-11 14:30:04 +0900

    Add consumers.py

---
+++ django_server/app_service/websocket_service/consumers.py
@@ -0,0 +1,30 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+
+
+class BusConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        pass
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        # Do something with the message
+
+
+class TrainConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        pass
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        # Do something with the message
