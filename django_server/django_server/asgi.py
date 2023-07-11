80dfba29fdcff0feaa2e339321a56912655ccc47
haneujeen <eujeenhan@gmail.com>
2023-07-11 18:57:47 +0900

    Fix: Exception in ASGI application

--- django_server/django_server/asgi.py
+++ django_server/django_server/asgi.py
@@ -6,11 +6,16 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+import app_service.websocket_service.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_server.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": URLRouter(
+        app_service.websocket_service.routing.websocket_urlpatterns
+    ),
+})
