214929bdfc0373d3fa9cadc33ed9c8362a032bd7
haneujeen <eujeenhan@gmail.com>
2023-07-11 18:59:05 +0900

    Add tasks.py

---
+++ django_server/openapi_seoul_service/bus/tasks.py
@@ -0,0 +1,30 @@
+"""
+app_service/websocket_service/consumers/BusConsumer.update:
+
+class BusConsumer(AsyncWebsocketConsumer):
+    # ...
+
+    async def update(self):
+        while True:
+            bus = await get_bus(self.id, self.route_id)
+            await self.send(text_data=json.dumps(bus))
+            await asyncio.sleep(10)
+"""
+
+async def get_bus(id, route_id):
+
+    bus = {
+        'id' ,
+        'name' ,
+        'previous_station': ,
+        'station': ,
+        'next_station': ,
+        'desc': {
+            'travel_time': ,
+            'speed': ,
+            'is_las': ,
+            'plate_number': ,
+        },
+    }
+
+    return bus
