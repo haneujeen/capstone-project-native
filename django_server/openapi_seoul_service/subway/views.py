d1a76619a655ab90bd18fa2981e3777b79a36ce0
haneujeen <eujeenhan@gmail.com>
2023-07-12 12:10:31 +0900

    Fix API response not being parsed updated subway module’s `get_stations` view to handle inconsistency in the API response format

--- django_server/openapi_seoul_service/subway/views.py
+++ django_server/openapi_seoul_service/subway/views.py
@@ -6,57 +6,57 @@ from urllib.parse import unquote
 
 SEOUL_API_KEY = unquote(settings.SEOUL_API_KEY)
 
+
 @api_view(['GET'])
 def get_stations(request, query):
     try:
         url = f"http://openapi.seoul.go.kr:8088/{SEOUL_API_KEY}/json/SearchSTNBySubwayLineInfo/1/1/%20/{query}/%20"
-
         response = requests.get(url)
         data = response.json()
+        print(data)
+        print("Trying")
 
-        if data['RESULT'] and data['RESULT']['CODE'] == 'INFO-200':
-            # Result not found
-            # Handle query here
-            pass
+        # WARN: API is not consistent in its response format
+        if 'SearchSTNBySubwayLineInfo' in data:
+            if data['SearchSTNBySubwayLineInfo']['RESULT']['CODE'] == 'INFO-000':
+                print("before trying")
+                try:
+                    print("still trying")
+                    index = data['SearchSTNBySubwayLineInfo']['list_total_count']
+                    url = f"http://openapi.seoul.go.kr:8088/{SEOUL_API_KEY}/json/SearchSTNBySubwayLineInfo/1/{index}/%20/{query}/%20"
+                    response = requests.get(url)
+                    data = response.json()['SearchSTNBySubwayLineInfo']
+                    stations = []
+                    for item in data['row']:
+                        station_item = {
+                            'id': item['STATION_CD'],
+                            'name': item['STATION_NM'],
+                            'line': item['LINE_NUM'],
+                            'type': 'subway',
+                        }
+                        stations.append(station_item)
 
-        if data['SearchSTNBySubwayLineInfo'] and data['SearchSTNBySubwayLineInfo']['RESULT']['CODE'] == 'INFO-000':
-            try:
-                index = data['SearchSTNBySubwayLineInfo']['list_total_count']
-
-                url = f"http://openapi.seoul.go.kr:8088/{SEOUL_API_KEY}/json/SearchSTNBySubwayLineInfo/1/{index}/%20/{query}/%20"
-
-                response = requests.get(url)
-                data = response.json()['SearchSTNBySubwayLineInfo']
-
-                stations = []
-                for item in data['row']:
-                    station_item = {
-                        'id': item['STATION_CD'],
-                        'name': item['STATION_NM'],
-                        'line': item['LINE_NUM'],
-                        'type': 'subway',
+                    station_response = {
+                        'is_valid': True,
+                        'response_code': '0',
+                        'message': data['RESULT']['MESSAGE'],
+                        'list': stations
                     }
-                    stations.append(station_item)
 
-                station_response = {
-                    'is_valid': True,
-                    'response_code': '0',
-                    'message': data['SearchSTNBySubwayLineInfo']['RESULT']['MESSAGE'],
-                    'list': stations
-                }
+                    return Response(station_response)
 
-                return Response(station_response)
+                except Exception as e:
+                    print(f"Exception occurred: {e}")
+                    return Response({"error": str(e)}, status=502)
 
-            except Exception as e:
-                return Response({"error": str(e)}, status=502)
-        else:
+        elif 'RESULT' in data:
             station_response = {
                 'is_valid': False,
                 'response_code': '202',
                 'message': data['RESULT']['MESSAGE'],
             }
-
             return Response(station_response)
 
     except Exception as e:
+        print(f"Exception occurred: {e}")
         return Response({"error": str(e)}, status=500)
--- react-app/screens/StationListScreen.js
+++ react-app/screens/StationListScreen.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faShuttleVan, faSubway } from '@fortawesome/free-solid-svg-icons';
 import { fetchBusArrivals, fetchStationsOnRoute } from '../api/api_service';
 
-// It's map-like screen because it shows the list of stations. But it's not a map.
 export default function StationListScreen({ route, navigation }) {
-    const { stations, message } = route.params;
+    const { busStations, busMessage, subwayStations, subwayMessage } = route.params;
+    const [stationType, setStationType] = useState('subway');
     const [departure, setDeparture] = useState(null);
-    const [stationType, setStationType] = useState(null);
     const [arrivals, setArrivals] = useState(null);
-    
+    const [message, setMessage] = useState(null);
+
     useEffect(() => {
         if (arrivals) {
             if (stationType === 'bus') {
@@ -19,41 +21,86 @@ export default function StationListScreen({ route, navigation }) {
         }
     }, [arrivals]);
 
+    const handleIconPress = (type) => {
+        setStationType(type);
+    }
+
     const selectDeparture = async (station) => {
         setDeparture(station);
         setStationType(station.type);
-        
-        let result, message;
+
+        let result;
         if (station.type === 'bus') {
             result = await fetchBusArrivals(station.ars_id);
-            if (result.response_code === '0') { // data.list[0].id
+            if (result.response_code === '0') {
                 setArrivals(result.list);
             } else {
-                message = result.message;
+                setMessage(result.message);
             }
 
         } else {
-            //const trainArrivals = fetchTrainArrivals(station.id);
-            //setArrivals(trainArrivals);
+            // const trainArrivals = await fetchTrainArrivals(station.id);
+            // setArrivals(trainArrivals);
         }
     };
 
     return (
         <View style={styles.container}>
-            <Text>From {departure?.name}</Text>
-            {stations && (
-                <FlatList
-                    data={stations}
-                    keyExtractor={(item) => item.id.toString()}
-                    renderItem={({ item }) => (
-                        <TouchableOpacity onPress={() => selectDeparture(item)}>
-                            <Text>{item.id} {item.name} {item.ars_id}</Text>
-                        </TouchableOpacity>
-                    )}
-                />
+            <View style={styles.innerContainer}>
+                <TouchableOpacity
+                    style={styles.iconContainer}
+                    onPress={() => handleIconPress('bus')}
+                    activeOpacity={0.6}
+                >
+                    <FontAwesomeIcon
+                        icon={faShuttleVan}
+                        size={30}
+                        style={{ color: stationType === 'bus' ? 'hsla(0, 0%, 20%, 1)' : 'hsla(0, 0%, 80%, 1)' }}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.iconContainer}
+                    onPress={() => handleIconPress('subway')}
+                    activeOpacity={0.6}
+                >
+                    <FontAwesomeIcon
+                        icon={faSubway}
+                        size={30}
+                        style={{ color: stationType === 'subway' ? 'hsla(0, 0%, 20%, 1)' : 'hsla(0, 0%, 80%, 1)' }}
+                    />
+                </TouchableOpacity>
+            </View>
+            {stationType === 'bus' && busStations && (
+                <View>
+                    <FlatList
+                        data={busStations}
+                        keyExtractor={(item) => item.id.toString()}
+                        renderItem={({ item }) => (
+                            <TouchableOpacity onPress={() => selectDeparture(item)}>
+                                <Text>{item.id} {item.name} {item.ars_id}</Text>
+                            </TouchableOpacity>
+                        )}
+                    />
+                </View>
             )}
-            {message && (
-                <Text>Wrong response: {message}</Text>
+            {stationType === 'bus' && busMessage && (
+                <Text>{busMessage}</Text>
+            )}
+            {stationType === 'subway' && busStations && (
+                <View>
+                    <FlatList
+                        data={subwayStations}
+                        keyExtractor={(item) => item.id.toString()}
+                        renderItem={({ item }) => (
+                            <TouchableOpacity onPress={() => selectDeparture(item)}>
+                                <Text>{item.id} {item.name} {item.line}</Text>
+                            </TouchableOpacity>
+                        )}
+                    />
+                </View>
+            )}
+            {stationType === 'subway' && subwayMessage && (
+                <Text>{subwayMessage}</Text>
             )}
         </View>
     );
@@ -62,8 +109,31 @@ export default function StationListScreen({ route, navigation }) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: 'hsla(240, 11%, 96%, 1)',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    innerContainer: {
+        flexDirection: 'row',
+        backgroundColor: 'hsla(0, 0%, 100%, 1)',
+        borderWidth: 1,
+        borderColor: 'hsla(0, 0%, 84%, 1)',
+        borderRadius: 10,
+        width: '80%',
+        height: '23%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+    },
+    iconContainer: {
+        backgroundColor: 'hsla(0, 0%, 100%, 1)',
+        borderWidth: 1,
+        borderColor: 'hsla(0, 0%, 84%, 1)',
+        borderRadius: 10,
+        width: '45%',
+        aspectRatio: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 7,
     },
 });
