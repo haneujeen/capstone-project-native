83f29e7bcd766ca2f8407f29ba5ac2b551b699bb
haneujeen <eujeenhan@gmail.com>
2023-07-17 06:59:48 +0900

    Update django command to use utils.py populate_subway_stations

--- django_server/openapi_seoul_service/management/commands/populate_subway_stations.py
+++ django_server/openapi_seoul_service/management/commands/populate_subway_stations.py
@@ -1,34 +1,13 @@
-import requests
-from django.conf import settings
 from django.core.management.base import BaseCommand
-from openapi_seoul_service.models import SubwayStation
-from urllib.parse import unquote
+from ...utils import populate_subway_stations
 
-SWOPENAPI_KEY = unquote(settings.SWOPENAPI_KEY)
 
 class Command(BaseCommand):
     help = 'Populate subway stations from the API'
 
     def handle(self, *args, **options):
-        url = f"http://swopenAPI.seoul.go.kr/api/subway/{SWOPENAPI_KEY}/json/realtimeStationArrival/ALL"
-
-        try:
-            response = requests.get(url)
-            data = response.json()
-
-            if 'realtimeArrivalList' in data:
-                data = response.json()['realtimeArrivalList']
-
-                for item in data:
-                    SubwayStation.objects.update_or_create(
-                        id=item['statnId'],
-                        defaults={
-                            'name': item['statnNm'],
-                            'direction': item['updnLine'],
-                            'line': item['subwayId'],
-                            'transfer_available': item['subwayList'].split(',')
-                        }
-                    )
-        except Exception as e:
-            self.stdout.write(self.style.ERROR(f'Error occurred: {e}'))
-
+        result = populate_subway_stations()
+        if "Error occurred" in result:
+            self.stdout.write(self.style.ERROR(result))
+        else:
+            self.stdout.write(self.style.SUCCESS(result))
