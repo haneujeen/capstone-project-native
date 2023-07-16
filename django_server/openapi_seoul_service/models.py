721d7abfa232760dfcd48fb2a2c8df5b7ec75df2
haneujeen <eujeenhan@gmail.com>
2023-07-17 06:57:37 +0900

    Update `SubwayStation` model: add next_station and update type of fields station_id and line add unique_together constraint update __str__ method

--- django_server/openapi_seoul_service/models.py
+++ django_server/openapi_seoul_service/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 # Create your models here.
 class SubwayStation(models.Model):
-    id = models.CharField(max_length=100, primary_key=True)
+    station_id = models.IntegerField()
     name = models.CharField(max_length=100)
     direction = models.CharField(max_length=50)
-    line = models.CharField(max_length=50)
-    transfer_available = models.JSONField()  # This field will store a list of lines
+    next_station = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
+    line = models.IntegerField()
+    transfer_available = models.JSONField(default=list)
+
+    class Meta:
+        unique_together = [('station_id', 'direction')]
 
     def __str__(self):
-        return self.name
+        return f'{self.name}-{self.station_id}-{self.direction}'
