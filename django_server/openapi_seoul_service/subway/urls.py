1c0e7e7f73fcdb912696ec01b0f176714df6f266
haneujeen <eujeenhan@gmail.com>
2023-07-17 12:01:37 +0900

    Update subway/urls.py

--- django_server/openapi_seoul_service/subway/urls.py
+++ django_server/openapi_seoul_service/subway/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('get_stations/<str:search_text>', views.get_stations),
     path('get_train/<int:number>', views.get_train),
-    path('get_stations_on_route/<str:name>/<str:line>/<str:direction>/<str:stops_at>/', views.get_stations_on_route),
+    path('get_stations_on_route/<str:start_name>/<str:line>/<str:direction>/<str:stops_at>/', views.get_stations_on_route),
 ]
