dc22bec98bad8ddf55b493965dd3d15f0a993837
haneujeen <eujeenhan@gmail.com>
2023-07-27 02:26:27 +0900

    Add `getSubwaySocket` function

--- react-app/api/socket_service.js
+++ react-app/api/socket_service.js
@@ -1,25 +1,25 @@
 export function getBusSocket(location, setBus) {
-    let endpoint = `ws://172.30.1.59:8000/ws/bus/${location.x, location.y}/`;
+    let endpoint = `ws://172.30.1.59:8000/ws/bus/${location.x}/${location.y}/`;
 
-    const socket = new WebSocket(endpoint);
+    let socket = new WebSocket(endpoint);
 
-    socket.addEventListener('open', (event) => {
-        console.log('WebSocket connection opened:', event);
-    });
+    socket.onopen = () => {
+        console.log('WebSocket is open now.');
+    };
 
-    socket.addEventListener('message', (event) => {
+    socket.onmessage = (event) => {
         console.log('Message from server: ', event.data);
         const data = JSON.parse(event.data);
         setBus(data);
-    });
+    };
 
-    socket.addEventListener('error', (event) => {
-        console.error('WebSocket error:', event);
-    });
+    socket.onerror = (error) => {
+        console.log(error);
+    };
 
-    socket.addEventListener('close', (event) => {
-        console.log('WebSocket connection closed:', event);
-    });
+    socket.onclose = (event) => {
+        console.log('WebSocket is closed now.', event.reason);
+    };
 
     const sendStopRequest = (request) => {
         const requestData = JSON.stringify(request);
@@ -27,6 +27,38 @@ export function getBusSocket(location, setBus) {
     }
 
     socket.sendStopRequest = sendStopRequest;
+
+    return socket;
+}
+
+export function getSubwaySocket(trainId, setTrain) {
+    let socket = new WebSocket(`ws:/172.30.1.59:8000/ws/subway/${trainId}/`);
+
+    socket.onopen = () => {
+        console.log('WebSocket is open now.');
+    };
+
+    socket.onmessage = (event) => {
+        console.log('Message from server: ', event.data);
+
+        let data;
+        if (event.data === 'null') {
+            data = JSON.parse(dummyTrain);
+        } else {
+            data = JSON.parse(event.data);
+        }
+
+        setTrain(data);
+    };
+
+    socket.onerror = (error) => {
+        console.log(error);
+    };
+
+    socket.onclose = (event) => {
+        console.log('WebSocket is closed now.', event.reason);
+        //setTimeout(() => getSubwaySocket(trainId, setTrain), 5000);
+    };
 
     return socket;
 }
